d693df7724fe80faeb2cbbe7cbd96b5a099d343e
ArlayshaO <71731654+ArlayshaO@users.noreply.github.com>
2021-02-01 16:08:50 -0600

    update card photos

--- Public/map.js
+++ Public/map.js
@@ -4,7 +4,7 @@
   let map;
    map = new google.maps.Map(document.getElementById("map"), {
      center: { lat: 39.099724, lng: -94.578331 },
-     zoom: 11,
+     zoom: 10,
      mapTypeId: 'hybrid'
    });
   }
